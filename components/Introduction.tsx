d9231b3116502bf996ecf0fc9166068565502a20
dhruvywuvy <156209341+dhruvywuvy@users.noreply.github.com>
2024-12-27 03:22:17 +0800

    fixing punctuation

--- components/Introduction.tsx
+++ components/Introduction.tsx
@@ -2,17 +2,17 @@ export default function Introduction() {
   return (
     <section className="mb-16 space-y-4">
   <p className="text-base text-[#333]">
-    👋 hey there! i'm dhruv, a passionate creator on a journey of endless exploration! 🚀 my adventure began at 14 as a freelance photographer, capturing the beauty of my hometown singapore and sharing it with millions online. 🎨
+    👋 hey there! i&apos;m dhruv, a passionate creator on a journey of endless exploration! 🚀 my adventure began at 14 as a freelance photographer, capturing the beauty of my hometown singapore and sharing it with millions online. 🎨
 
     <br></br><br></br>
 
-    since then, i've dived into various exciting fields. i've produced music 🎵, served in the air force 🛩️, and developed software for the Fortune 500. 
+    since then, i&apos;ve dived into various exciting fields. i&apos;ve produced music 🎵, served in the air force 🛩️, and developed software for the Fortune 500. 
     
     <br></br><br></br>
 
     🏥 currently, my team and i are pouring our hearts into something truly meaningful. 
     inspired by my own experiences with injuries during military service + conversations with people navigating the maze that is the healthcare system,
-    we're building advocare so that no one ever overpays for medical bills again 💸 🚫.
+    we&apos;re building advocare so that no one ever overpays for medical bills again 💸 🚫.
   </p>
 </section>
 
